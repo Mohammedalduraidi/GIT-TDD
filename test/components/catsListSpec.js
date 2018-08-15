3b35bcf913a38b8a58c3e0df007c035f477cc42e
Mohammedalduraidi <mohd.alduraidi@gmail.com>
2018-08-15 16:47:05 +0300

    (create) front end test

--- test/components/catsListSpec.js
+++ test/components/catsListSpec.js
@@ -1,1 +1,42 @@
 // TODO: Your code here
+
+'use strict'
+
+/*
+ * Unit tests for lib/calculator.js
+ */
+
+
+
+describe('Adding', function () {
+   
+        var myApp;
+
+        beforeEach(function () {
+            myApp = angular.module('catsList');
+        });
+
+        it('should have a catsCtrl controller', function () {
+            expect(myApp).toBeDefined();
+            expect(myApp.controller('catsCtrl')).toBeDefined();
+        });
+
+
+        it('Why should I  make a test ? ', function () {
+
+
+
+        });
+
+
+
+        it('Should I really make a test ?! ', function () {
+
+
+
+        });
+
+
+    });
+
+   
